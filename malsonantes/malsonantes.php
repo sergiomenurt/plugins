60156dc4d04eec397a67f87270f950f4a25a3794
sergiomenurt <smendezurtaza@danielcastelao.org>
2022-12-19 10:49:27 +0100

    Initial commit

---
+++ malsonantes/malsonantes.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Palabras Malsonantes
+
+Plugin URI: http://wordpress.org/plugins/malsonantes/
+
+Description: Este plugin cambia unas palabras malsonantes por otras
+
+Author: Sergio Mendez
+
+Version: 1.0
+
+Author URI: http://jack.sir/
+*/
+
+function cambiar_malsonantes( $text){
+
+    $palabrasMalsonantes= ["mierda","jodido","follón","lerdo","nabo"];
+
+    $cambios = ["caca","duro","problema","bobo","pene"];
+
+    return str_replace($palabrasMalsonantes , $cambios, $text);
+}
+/*
+ * Cambia el contenido del post
+ */
+
+add_filter('the_content', 'cambiar_malsonantes');
